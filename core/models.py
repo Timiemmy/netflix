a9a57ed983179629dd1d010d6e853cf582032276
Timiemmy <faith.bolanle@yahoo.com>
2023-03-31 18:55:32 +0100

    Update requirements and added core models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+AGE_CHOICES = (
+    ('All', 'All'),  # The first will be in the db and the second will be shown to users
+    ('Kids', 'Kids')
+)
+
+MOVIE_CHOICES = (
+    ('Seasonal', 'Seasonal'),
+    ('Single', 'Single')
+)
+
+
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyField('Profile', null=True, blank=True)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    age_limit = models.CharField(max_length=10, choices=AGE_CHOICES)
+    uuid = models.UUIDField(default=uuid.uuid4)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4)
+    type = models.CharField(max_length=10, choices=MOVIE_CHOICES)
+    movie = models.ManyToManyField('Video')
+    flyer = models.ImageField(upload_to='flyers')
+    age_limit = models.CharField(max_length=10, choices=AGE_CHOICES)
