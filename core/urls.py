79718795e670fe88cc98a30cf4ecafc0f19c8fde
Timiemmy <faith.bolanle@yahoo.com>
2023-04-02 18:13:05 +0100

    Added views and added neccessary templates and routing

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from .views import Home, ProfileList, ProfileCreate, Watch
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('profile/', ProfileList.as_view(), name='profile_list'),
+    path('profile/create/', ProfileCreate.as_view(), name='profile_create'),
+    path('watch/<str:profile_id>/', Watch.as_view(), name='watch'),
 ]
